a64cdc55f84455d7ce4f365027113cf081cc7edf
DebdutChowdhury <debdut.chowdhury2018@gmail.com>
2021-01-31 00:22:46 +0530

    Added: we add one email here for validation test

--- EmailRegex.java
+++ EmailRegex.java
@@ -1,5 +1,21 @@
-public class EmailRegex{
-	public static void main(String[] args){
-		System.out.println("Welcome, here we are test email validation");
-	}
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class EmailRegex {
+    public static boolean IsEmailMatch(String email){
+        Pattern pattern = Pattern.compile("^(.+)@(.+)$");
+        Matcher matcher = pattern.matcher(email);
+        boolean result = matcher.matches();
+//        System.out.println(result);
+        return result;
+    }
+    public static void main(String[] args) {
+        System.out.println("Valid Email using regex:");
+        System.out.println("---------------------------");
+
+        String p1 = "abc.xyz@bridgelabz.co.in";
+        System.out.println(p1 + " ==> " +IsEmailMatch(p1));
+    }
 }
+
+
